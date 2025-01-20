e220e778e0cbe2f77b93b4dcbf3dd0abf391a140
Shrikant-kshatriya <shrikant070920@gmail.com>
2025-01-20 17:54:47 +0530

    final commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,9 @@ import './index.css'
 import App from './App.jsx'
 
 // axios defaults
+
 import axios from 'axios';
-axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL; 
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
