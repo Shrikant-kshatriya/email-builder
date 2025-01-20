c77b5e09e878862c360e1ad65d3d049b8b36715b
Shrikant-kshatriya <shrikant070920@gmail.com>
2025-01-20 19:16:31 +0530

    server cors updated

--- server/index.js
+++ server/index.js
@@ -8,23 +8,10 @@ const app = express();
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // cors 
-const allowedOrigins = [
-    'https://email-builder-server-self.vercel.app',
-    'http://localhost:5173'
-];
-
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}));
-
+    origin: ['https://email-builder-frontend-tau.vercel.app','http://localhost:5173'], 
+    credentials: true, 
+})); 
 
 
 app.use(express.json());
