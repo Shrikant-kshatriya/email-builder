537069e05a4e478fbd2a80d62db851577d5f3f69
Shrikant-kshatriya <shrikant070920@gmail.com>
2025-01-20 17:46:40 +0530

    vercel edit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // cors 
 app.use(cors({
-    origin: ['http://localhost:5173', '*'], 
+    origin: ['https://email-builder-frontend-tau.vercel.app','http://localhost:5173', '*'], 
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE'] 
  
