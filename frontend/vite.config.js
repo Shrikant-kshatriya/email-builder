77f3355455d7dc332918d6e3924f997d7f958c8e
Shrikant-kshatriya <shrikant070920@gmail.com>
2025-01-20 19:01:43 +0530

    frontend vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: './', // Ensure the app works with relative paths
+  build: {
+    outDir: 'dist', // Default build output folder
+  },
 })
